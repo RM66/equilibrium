73637cefd0d377f2dfcc25d4ce7e6f9613df833a
Roma Y <roman-yaml@yandex-team.ru>
2021-02-01 23:10:46 +0500

    logic

--- src/script.js
+++ src/script.js
@@ -18,11 +18,35 @@
     tooltips: true,
   };
 
+  const values = new Array(15);
+
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioCtx.createOscillator();
+  const gain = audioCtx.createGain();
+  let started = false;
+
+  oscillator.connect(gain);
+  gain.connect(audioCtx.destination);
+
   sliders.forEach((el, i) => {
     noUiSlider.create(el, options);
 
-    el.noUiSlider.on("update", ([val]) => {
-      console.log("value, index", +val, i);
+    el.noUiSlider.on("update", ([db]) => {
+      const vol = Math.pow(10, (db - 12) / 20);
+      gain.gain.value = values[i] = vol;
     });
+
+    const handle = el.noUiSlider.target.getElementsByClassName(
+      "noUi-handle"
+    )[0];
+    handle.addEventListener("focus", () => {
+      if (!started) {
+        oscillator.start();
+        started = true;
+      }
+      oscillator.frequency.value = el.dataset.freqnum;
+      gain.gain.value = values[i];
+    });
+    handle.addEventListener("blur", () => (gain.gain.value = 0));
   });
 })();
